29468a350ef437e49290ed31d67b0644c3b00dbc
JMDiaz <73129466+Jmdiaz1998@users.noreply.github.com>
2022-07-23 15:54:13 -0400

    adds button to change background color

--- MyFirstAppHelloWorld/app/src/main/java/com/example/myfirstapp_helloworld/MainActivity.kt
+++ MyFirstAppHelloWorld/app/src/main/java/com/example/myfirstapp_helloworld/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,14 +20,18 @@ class MainActivity : AppCompatActivity() {
 
         // Get a reference to button
         //2.Set up logic to know when user has tapped on the button
-        findViewById<Button>(R.id.button).setOnClickListener {
+        findViewById<Button>(R.id.change_text_color_button).setOnClickListener {
             // Handles button tap
             // 3.Change the color of the text
             Log.i("Jorge", "Tapped on button")
             // Get a reference to the text view
             // Set the color of the text view
             findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.pearl_white))
+        }
 
+        findViewById<Button>(R.id.change_background_color_button).setOnClickListener {
+            Log.i("Jorge", "Tapped on button2")
+            findViewById<ConstraintLayout>(R.id.backgroundView).setBackgroundColor(getResources().getColor(R.color.white))
         }
 
     }
