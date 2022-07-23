9478300bc30b57c4224d11fd46da6b43ce0796e5
JMDiaz <73129466+Jmdiaz1998@users.noreply.github.com>
2022-07-23 14:22:30 -0400

    Added prework files to repository

---
+++ MyFirstAppHelloWorld/app/src/main/java/com/example/myfirstapp_helloworld/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myfirstapp_helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Included in this app:
+        // Display customized to show 'Hello from {name}'
+        // Display customized background color
+        // Display button to change text color
+        // 1. Add a button
+
+        // Get a reference to button
+        //2.Set up logic to know when user has tapped on the button
+        findViewById<Button>(R.id.button).setOnClickListener {
+            // Handles button tap
+            // 3.Change the color of the text
+            Log.i("Jorge", "Tapped on button")
+            // Get a reference to the text view
+            // Set the color of the text view
+            findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.pearl_white))
+
+        }
+
+    }
+}
